0397feca16093d27afa593813378e5c367125f1e
iumanyue <582966488@qq.com>
2021-03-18 15:07:46 +0800

    撒花

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import FastClick from 'fastclick'
 
 import toast from 'components/common/toast/index.js'
 
+import VueLazyLoad from 'vue-lazyload'
+
 Vue.config.productionTip = false
 
 // vue实例是可以作为事件总线的。
@@ -17,6 +19,12 @@ Vue.use(toast)
 // 解决移动端300ms延迟
 
 FastClick.attach(document.body)
+
+// 使用懒加载
+Vue.use(VueLazyLoad,{
+  loading: require('./assets/img/common/placeholder.png')
+})
+
 
 new Vue({
   render: h => h(App),
